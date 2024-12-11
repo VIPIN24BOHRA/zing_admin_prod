124383fc4735b866bc04bca3c620380113017a37
Vipin Bohra <vipinbohra321@gmail.com>
2024-12-11 18:27:13 +0530

    feat: added auth context

---
+++ providers/authProvider/authContext.tsx
@@ -0,0 +1,124 @@
+'use client';
+import { createContext, useContext, useState, useEffect } from 'react';
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+  User
+} from 'firebase/auth';
+import { app } from '@/lib/db';
+import { getAuth } from 'firebase/auth';
+
+interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+  login: () => Promise<void>;
+  logout: () => Promise<boolean>;
+  setNotAllowed: any;
+  notAllowed: boolean;
+}
+
+const loginWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  try {
+    await signInWithPopup(auth, provider);
+  } catch (error) {
+    console.error('Google login failed:', error);
+  }
+};
+
+// Function to log out
+const logout = async () => {
+  try {
+    await signOut(auth);
+    return true;
+  } catch (error) {
+    console.error('Logout failed:', error);
+    return false;
+  }
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+const auth = getAuth(app);
+
+const verifyUser = async (token: string) => {
+  try {
+    const res = await fetch('/api/verifyAdmin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        apiKey: process.env.NEXT_PUBLIC_WEB_API_KEY,
+        token
+      })
+    });
+
+    const data = await res.json();
+    if (data.success) {
+      console.log('Login successful!');
+      return true;
+    } else {
+      console.log('Access denied. Only @domain.com emails are allowed.');
+      return false;
+    }
+  } catch (error) {
+    console.error('Error during sign-in:', error);
+    return false;
+  }
+};
+
+export const AuthProvider = ({ children }: any) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [notAllowed, setNotAllowed] = useState(false);
+  useEffect(() => {
+    console.log('Auth provider is called');
+
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      console.log('on auth state changed', user);
+
+      if (user) {
+        const token = await user.getIdToken();
+        const res = await verifyUser(token);
+        console.log(res);
+        if (res) setUser(user);
+        else {
+          setNotAllowed(true);
+        }
+      } else {
+        setUser(null);
+      }
+
+      setLoading(false);
+    });
+
+    return () => unsubscribe(); // Cleanup subscription
+  }, []);
+
+  const value: AuthContextType = {
+    user,
+    loading,
+    login: loginWithGoogle,
+    logout,
+    setNotAllowed,
+    notAllowed
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+// Custom hook to use the AuthContext
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
