de75e28e6719d0f101e8cca32054ef166d2d7cab
Vipin Bohra <vipinbohra321@gmail.com>
2025-01-25 14:49:04 +0530

    feat: added cashfree prod webhook

--- app/api/webhook/cashfree/route.ts
+++ app/api/webhook/cashfree/route.ts
@@ -1,5 +1,10 @@
 /* eslint-disable import/extensions */
-import { addNewPaymentDeatils } from 'modules/firebase/database';
+import {
+  createOrderTEST,
+  getPendingOrder,
+  getPendingOrderTEST,
+  savePaymentDetailsTEST
+} from 'modules/firebase/database';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
@@ -10,7 +15,45 @@ export async function POST(req: NextRequest) {
   try {
     const response = await req.json();
 
-    console.log(response);
+    if (
+      response &&
+      response.type == 'PAYMENT_SUCCESS_WEBHOOK' &&
+      !response.data?.payment_gateway_details
+    ) {
+      const { order, payment } = response.data;
+      console.log(order, payment);
+
+      const pendingOrder = await getPendingOrderTEST(order.order_id);
+
+      const transactionDetail = {
+        cfOrderId: payment.cf_payment_id,
+        orderId: order.order_id,
+        status:
+          payment.payment_status == 'SUCCESS' ? 'PAID' : payment.payment_status
+      };
+      console.log(pendingOrder, transactionDetail);
+
+      // create order.
+      await createOrderTEST(pendingOrder, transactionDetail);
+
+      await savePaymentDetailsTEST({
+        order,
+        payment,
+        orderStatus:
+          payment.payment_status == 'SUCCESS' ? 'PAID' : payment.payment_status
+      });
+
+      // save payment details in /payment/cashfree
+    } else if (response && response.type == 'PAYMENT_FAILED_WEBHOOK') {
+      const { order, payment } = response.data;
+      await savePaymentDetailsTEST({
+        order,
+        payment,
+        orderStatus:
+          payment.payment_status == 'SUCCESS' ? 'PAID' : payment.payment_status
+      });
+    }
+
     return NextResponse.json({
       success: true
     });
