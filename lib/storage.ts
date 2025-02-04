39a2cddd14814b30b0af3b03d8911ab301590879
Vipin Bohra <vipinbohra321@gmail.com>
2025-02-04 23:14:15 +0530

    fix: paid key in exports

--- lib/storage.ts
+++ lib/storage.ts
@@ -3,11 +3,14 @@ import {
   ref as storageRef,
   uploadBytes,
   getDownloadURL,
-  getMetadata,
+  getMetadata
 } from 'firebase/storage';
 import { app } from './db';
 
-export const uploadImage = async (key: string, file: File): Promise<string | null> => {
+export const uploadImage = async (
+  key: string,
+  file: File
+): Promise<string | null> => {
   console.log(key, file);
   if (!key || !file) return null;
 
@@ -15,17 +18,6 @@ export const uploadImage = async (key: string, file: File): Promise<string | nul
   const imageRef = storageRef(storage, `images/${key}`);
 
   try {
-    try {
-      await getMetadata(imageRef);
-      console.log(`File with key "${key}" already exists. Replacing it.`);
-    } catch (error: any) {
-      if (error.code === 'storage/object-not-found') {
-        console.log(`File with key "${key}" does not exist. Uploading it.`);
-      } else {
-        throw error;
-      }
-    }
-
     await uploadBytes(imageRef, file);
     console.log('File uploaded or replaced successfully.');
 
